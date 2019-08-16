84e938df670b32c1e55a6b62fef93c99b43a090d
Daniel Pan <danielcpan@gmail.com>
2019-08-15 22:57:26 -0700

    Separated Frontend from api

--- client/src/actions/errorActions.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { RESET_ERRORS } from '../constants/actionTypes';
-
-export const resetErrors = () => (dispatch) => dispatch({ type: RESET_ERRORS });
